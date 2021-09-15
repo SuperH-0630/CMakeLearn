650bf9c586e383ee3c35b42c1e260c97d18f33c8
SongZihuan <jimsong2010@163.com>
2021-09-15 23:28:18 +0800

    feat: 添加了测试程序

---
+++ test/test1.c
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Test1\n");
+    return EXIT_SUCCESS;
+}
---
+++ test/test_fail.c
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Test fail\n");
+    return EXIT_FAILURE;
+}
