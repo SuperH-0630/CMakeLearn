24e73e410cc76ddab4a9cbcfe6e22d62939da2a8
SongZihuan <jimsong2010@163.com>
2021-09-16 10:51:12 +0800

    feat: 添加安装程序

---
+++ cmake/DLLFind.cmake
@@ -0,0 +1,13 @@
+﻿#[[
+文件名: DllFind.cmake
+定位windows的dll文件用的
+]]
+
+function(dll_find re path)
+    file(GLOB_RECURSE _dll  # 遍历所有的.dll
+         LIST_DIRECTORIES FALSE  #
+         CONFIGURE_DEPENDS
+         "${path}/*.dll"
+         )
+    set(${re} ${_dll} PARENT_SCOPE)
+endfunction()
---
+++ cmake/install_info.cmake
@@ -0,0 +1,6 @@
+﻿#[[
+文件名: install_info.cmake
+目标: 一个install(SCRIPT)的测试脚本
+]]
+
+message(STATUS "install_info: CMake-Learn Installing")
